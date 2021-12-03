fe1f989d355cd9be05931740b3f2caa4168efadb
Per Rovegård <per@rovegard.se>
2021-12-03 07:03:21 +0100

    Day wrappers

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,17 +1,19 @@
 import java.io.File
 
-fun part1(numbers: List<Int>): Int {
-    return numbers.zipWithNext().count { it.second > it.first }
-}
+object Day1 {
+    fun part1(numbers: List<Int>): Int {
+        return numbers.zipWithNext().count { it.second > it.first }
+    }
 
-fun part2(numbers: List<Int>): Int {
-    return numbers.windowed(3).zipWithNext().count { it.second.sum() > it.first.sum() }
+    fun part2(numbers: List<Int>): Int {
+        return numbers.windowed(3).zipWithNext().count { it.second.sum() > it.first.sum() }
+    }
 }
 
 fun main(args: Array<String>) {
 
     val numbers = File("input/day1.txt").useLines { it.toList() }.map { Integer.parseInt(it) }
 
-    println("Part 1 = ${part1(numbers)}")
-    println("Part 2 = ${part2(numbers)}")
+    println("Part 1 = ${Day1.part1(numbers)}")
+    println("Part 2 = ${Day1.part2(numbers)}")
 }
--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,37 +1,41 @@
 import java.io.File
 
-fun d2_part1(commands: List<Command>): Int {
+object Day2 {
 
-    val (p, d) = commands.fold(Pair(0, 0)) { pair, cmd ->
-        val (p, d) = pair
+    fun part1(commands: List<Command>): Int {
 
-        when (cmd.op) {
-            "forward" -> Pair(p + cmd.units, d)
-            "up" -> Pair(p, d - cmd.units)
-            "down" -> Pair(p, d + cmd.units)
-            else -> throw Exception("Unknown cmd: ${cmd.op}")
+        val (p, d) = commands.fold(Pair(0, 0)) { pair, cmd ->
+            val (p, d) = pair
+
+            when (cmd.op) {
+                "forward" -> Pair(p + cmd.units, d)
+                "up" -> Pair(p, d - cmd.units)
+                "down" -> Pair(p, d + cmd.units)
+                else -> throw Exception("Unknown cmd: ${cmd.op}")
+            }
         }
+
+        return p * d
     }
 
-    return p * d
-}
+    fun part2(commands: List<Command>): Int {
 
-fun d2_part2(commands: List<Command>): Int {
+        val (p, d, _) = commands.fold(Triple(0, 0, 0)) { triple, cmd ->
+            val (p, d, a) = triple
 
-    val (p, d, _) = commands.fold(Triple(0, 0, 0)) { triple, cmd ->
-        val (p, d, a) = triple
-
-        when (cmd.op) {
-            "forward" -> Triple(p + cmd.units, d + a * cmd.units, a)
-            "up" -> Triple(p, d, a - cmd.units)
-            "down" -> Triple(p, d, a + cmd.units)
-            else -> throw Exception("Unknown cmd: ${cmd.op}")
+            when (cmd.op) {
+                "forward" -> Triple(p + cmd.units, d + a * cmd.units, a)
+                "up" -> Triple(p, d, a - cmd.units)
+                "down" -> Triple(p, d, a + cmd.units)
+                else -> throw Exception("Unknown cmd: ${cmd.op}")
+            }
         }
+
+        return p * d
     }
 
-    return p * d
+    data class Command(val op: String, val units: Int)
 }
-data class Command(val op: String, val units: Int)
 
 fun main(args: Array<String>) {
 
@@ -39,9 +43,9 @@ fun main(args: Array<String>) {
         val parts = it.split(" ")
         val op = parts[0]
         val units = Integer.parseInt(parts[1])
-        Command(op, units)
+        Day2.Command(op, units)
     }
 
-    println("Part 1 = ${d2_part1(commands)}")
-    println("Part 2 = ${d2_part2(commands)}")
+    println("Part 1 = ${Day2.part1(commands)}")
+    println("Part 2 = ${Day2.part2(commands)}")
 }
