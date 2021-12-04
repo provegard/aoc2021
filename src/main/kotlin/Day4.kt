c5a88db34d2866ea74ecd78aa67e42ab87c8af6d
Per Rovegård <per@rovegard.se>
2021-12-04 06:45:54 +0100

    Day 4 part 1

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,85 @@
+object Day4 {
+
+    class Board(private val state: List<List<Int>>) {
+
+        private constructor(state: List<List<Int>>, mn: Set<Int>) : this(state) {
+            this.markedNumbers = mn
+        }
+
+        private var markedNumbers: Set<Int> = emptySet()
+
+        fun markNumber(n: Int): Board {
+            val newSet = markedNumbers.plus(n)
+            return Board(state, newSet)
+        }
+
+        fun isWin(): Boolean {
+            return listOf(state, columns().toList()).flatten().any { it.all { n -> markedNumbers.contains(n) } }
+        }
+
+        fun unmarkedSum(): Int {
+            return listOf(state, columns().toList()).flatten().flatten().filter { !markedNumbers.contains(it) }.distinct().sum()
+        }
+
+        private fun columns(): Sequence<List<Int>> = sequence {
+            // Assume square
+            for (i in state.indices) {
+                yield(state.map { it[i] })
+            }
+        }
+    }
+
+    class World(private val boards: List<Board>, private val numbers: List<Int>, private val prevNumber: Int) {
+
+        fun hasWinner(): Boolean {
+            return boards.any { it.isWin() }
+        }
+
+        fun calculate(): Int {
+            val winner = boards.first { it.isWin() }
+            return winner.unmarkedSum() * prevNumber
+        }
+
+        fun step(): World {
+            val num = numbers.first()
+            val newBoards = boards.map { it.markNumber(num) }
+            return World(newBoards, numbers.drop(1), num)
+        }
+    }
+
+    fun readBoardLines(lines: List<String>): List<List<Int>> {
+        if (lines.isEmpty()) return emptyList()
+        val line = lines.first().trim()
+        if (line == "") return emptyList()
+        val numbers = line.split("[ ]+".toRegex()).map { it.toInt() }
+        return listOf(numbers).plus(readBoardLines(lines.drop(1)))
+    }
+
+    fun readBoards(lines: List<String>): List<Board> {
+        if (lines.isEmpty()) return emptyList()
+        val boardLines = readBoardLines(lines)
+        val next = lines.drop(1 + boardLines.size)
+        return listOf(Board(boardLines)).plus(readBoards(next))
+    }
+
+    fun createWorld(lines: List<String>): World {
+        val numbers = lines.first().split(",").map { it.toInt() }
+        val boards = readBoards(lines.drop(1))
+        return World(boards, numbers, -1)
+    }
+
+    fun part1(lines: List<String>): Int {
+        var world = createWorld(lines)
+        while (!world.hasWinner()) {
+            world = world.step()
+        }
+        return world.calculate()
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val lines = readLines("day4")
+
+    println("Part 1 = ${Day4.part1(lines)}")
+}
